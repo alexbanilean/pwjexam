1c1103b7860bad37882c2309043120480e99e48c
alexbanilean <alexandru.banilean@gmail.com>
2025-01-30 12:48:41 +0200

    feat: initial commit

---
+++ src/main/java/com/unibuc/pwjexam/PwjexamApplication.java
@@ -0,0 +1,13 @@
+package com.unibuc.pwjexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwjexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwjexamApplication.class, args);
+	}
+
+}
